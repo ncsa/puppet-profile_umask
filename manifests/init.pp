a3c0f8e55fd21ed88a32bd6b92604497cdb89193
David King <kingda@illinois.edu>
2021-11-05 14:26:29 -0500

    dont duplicate resource statement

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,13 @@ class profile_umask (
   String $loginumask,
 ) {
   ##Change default user umask
-  file_line { 'default_user_umask':
+  file_line { 'default_user_umask_profile':
     path               => '/etc/profile',
     line               => "    umask ${loginumask}",
     match              => '^\s*umask\s022$',
     append_on_no_match => 'false',
   }
-  file_line{ 'default_user_umask':
+  file_line{ 'default_user_umask_logins.def':
     path               => '/etc/profile',
     line               => "UMASK           ${loginumask}",
     match              => '^UMASK\s*022',
